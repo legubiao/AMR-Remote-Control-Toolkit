6a99a35c1dbf6f500c38752de4a4d5e04e98fa5f
biao <biao876990970@hotmail.com>
2024-03-14 15:20:02 +0800

    增加了选择地图的功能

--- scripts/mapping_control.py
+++ scripts/mapping_control.py
@@ -4,15 +4,22 @@ import rospy
 import subprocess
 from std_msgs.msg import String
 
+def launch_amcl(map_name):
+    amcl_command = "roslaunch amr_platform amcl.launch map_file:=" + foldername+'/'+map_name + ".yaml"
+    rospy.logwarn(amcl_command)
+    process = subprocess.Popen(amcl_command.split())
+    return process
+
 def command_callback(msg):
     global mapping_process
+    global amcl_process
     global isMapping
     command = msg.data.split()
 
     if command[0] == "start_mapping":
 
         if isMapping:
-            rospy.WARN("Already in mapping status")
+            rospy.logwarn("Already in mapping status")
         else:
             # 启动SLAM建图
             mapping_process = subprocess.Popen(start_mapping_command.split())
@@ -32,8 +39,17 @@ def command_callback(msg):
             mapping_process.terminate()
             rospy.loginfo("SLAM mapping process terminated")
             isMapping = False
+
+            # 启动AMCL节点
+            amcl_process = launch_amcl(command[1])
         else:
-            rospy.WARN("Not mapping yet")
+            rospy.logwarn("Not mapping yet")
+
+    elif command[0] == "load":
+            isMapping = False
+            # 启动AMCL节点
+            amcl_process = launch_amcl(command[1])
+            rospy.loginfo("Load map file: " + command[1])
 
 if __name__ == "__main__":
     rospy.init_node("mapping_node")
