f5d46821aba5724b3d5559fe5aed7f2e23e10039
Huang Zhenbiao <biao876990970@hotmail.com>
2024-05-03 16:00:44 +0800

    add trajectory visualize

--- launch/turtlebot.launch.py
+++ launch/turtlebot.launch.py
@@ -8,12 +8,15 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     webots_ros2_turtlebot_dir = get_package_share_directory('webots_ros2_turtlebot')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
     return LaunchDescription([
         Node(
             name='scan_tf_republisher',
             package='tf2_ros',
             executable='static_transform_publisher',
             arguments=['-0.032', '0', '0.172', '0', '0', '0', 'base_link', 'LDS-01'],
+            parameters=[{'use_sim_time': use_sim_time}],
         ),
 
         Node(
@@ -21,6 +24,7 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             arguments=['-0.032', '0', '0.068', '0', '0', '0', 'base_link', 'imu_link'],
+            parameters=[{'use_sim_time': use_sim_time}],
         ),
 
         IncludeLaunchDescription(
--- src/pose_publisher.cpp
+++ src/pose_publisher.cpp
@@ -5,6 +5,34 @@
 #include <tf2_ros/buffer.h>
 #include <tf2/LinearMath/Transform.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <deque>
+#include <nav_msgs/msg/path.hpp>
+#include <amr_rctk/srv/frame_to_pose.hpp>
+
+bool handle_frame_to_pose_request(
+    const std::shared_ptr<amr_rctk::srv::FrameToPose::Request> request,
+    std::shared_ptr<amr_rctk::srv::FrameToPose::Response> response,
+    tf2_ros::Buffer& tfBuffer,
+    const rclcpp::Node::SharedPtr& node)
+{
+    geometry_msgs::msg::TransformStamped transformStamped;
+    try{
+        transformStamped = tfBuffer.lookupTransform(request->frame1, request->frame2, rclcpp::Time(0));
+    }
+    catch (tf2::TransformException &ex) {
+        RCLCPP_WARN(node->get_logger(), "%s", ex.what());
+        return false;
+    }
+
+    response->pose.header.stamp = node->now();
+    response->pose.header.frame_id = request->frame1;
+    response->pose.pose.position.x = transformStamped.transform.translation.x;
+    response->pose.pose.position.y = transformStamped.transform.translation.y;
+    response->pose.pose.position.z = transformStamped.transform.translation.z;
+    response->pose.pose.orientation = transformStamped.transform.rotation;
+
+    return true;
+}
 
 int main(int argc, char** argv){
     rclcpp::init(argc, argv);
@@ -12,36 +40,57 @@ int main(int argc, char** argv){
     auto node = std::make_shared<rclcpp::Node>("robot_pose_publisher");
 
     auto pose_pub = node->create_publisher<geometry_msgs::msg::PoseStamped>("/robot_pose", 10);
+    auto path_pub = node->create_publisher<nav_msgs::msg::Path>("/robot_path", 10);
 
     tf2_ros::Buffer tfBuffer(node->get_clock());
     tf2_ros::TransformListener tfListener(tfBuffer);
 
+    auto service = node->create_service<amr_rctk::srv::FrameToPose>("frame_to_pose",
+        std::bind(&handle_frame_to_pose_request, std::placeholders::_1, std::placeholders::_2, std::ref(tfBuffer), node));
+
+
     bool print_error = node->declare_parameter<bool>("print_error", false);
+
+    std::deque<geometry_msgs::msg::PoseStamped> poses;
+    nav_msgs::msg::Path path_msg;
+    path_msg.header.frame_id = "map";
+    int path_counter = 5;
 
     rclcpp::Rate rate(10.0);
     while (rclcpp::ok()){
         geometry_msgs::msg::TransformStamped transformStamped;
         try{
             transformStamped = tfBuffer.lookupTransform("map", "base_link", tf2::TimePointZero);
+            geometry_msgs::msg::PoseStamped pose_msg;
+            pose_msg.header.stamp = node->now();
+            pose_msg.header.frame_id = "map";
+            pose_msg.pose.position.x = transformStamped.transform.translation.x;
+            pose_msg.pose.position.y = transformStamped.transform.translation.y;
+            pose_msg.pose.position.z = transformStamped.transform.translation.z;
+            pose_msg.pose.orientation = transformStamped.transform.rotation;
+
+            pose_pub->publish(pose_msg);
+
+            path_counter -= 1;
+            if (path_counter == 0){
+                path_counter = 5;
+                poses.push_back(pose_msg);
+                if (poses.size() > 100){
+                    poses.pop_front();
+                }
+                path_msg.poses = std::vector<geometry_msgs::msg::PoseStamped>(poses.begin(), poses.end());
+                path_msg.header.stamp = node->now();
+                path_msg.header.frame_id = "map";
+                path_pub->publish(path_msg);
+            }
         }
         catch (tf2::TransformException &ex) {
             if (print_error){
                 RCLCPP_ERROR(node->get_logger(), "%s", ex.what());
             }
-            rclcpp::sleep_for(std::chrono::seconds(1));
-            continue;
         }
 
-        geometry_msgs::msg::PoseStamped pose_msg;
-        pose_msg.header.stamp = node->now();
-        pose_msg.header.frame_id = "map";
-        pose_msg.pose.position.x = transformStamped.transform.translation.x;
-        pose_msg.pose.position.y = transformStamped.transform.translation.y;
-        pose_msg.pose.position.z = transformStamped.transform.translation.z;
-        pose_msg.pose.orientation = transformStamped.transform.rotation;
-
-        pose_pub->publish(pose_msg);
-
+        rclcpp::spin_some(node);
         rate.sleep();
     }
     rclcpp::shutdown();
